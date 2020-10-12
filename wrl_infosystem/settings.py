61c3a7e9fcfcb39826e6e5a6e7251bc9e5e2ff5b
wesleycomp <weslleycomp@gmail.com>
2020-10-12 17:57:58 -0300

    inserindo authenticao 2

--- wrl_infosystem/settings.py
+++ wrl_infosystem/settings.py
@@ -52,8 +52,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'wrl_infosystem.urls'
 
-#LOGIN_REDIRECT_URL = "index"   # Route defined in clinica/urls.py
-#LOGOUT_REDIRECT_URL = "index"  # Route defined in clinica/urls.py
+LOGIN_REDIRECT_URL = "index"   # Route defined in clinica/urls.py
+LOGOUT_REDIRECT_URL = "index"  # Route defined in clinica/urls.py
 TEMPLATE_DIR = os.path.join(BASE_DIR, "clinica/templates")  # ROOT dir for templates
 
 TEMPLATES = [
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'wrl_infosystem.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -94,7 +94,7 @@ DATABASES = {
     'default': dj_database_url.config()
 }
 
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
