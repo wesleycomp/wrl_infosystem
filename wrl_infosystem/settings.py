84f9242a529f860d639ce559e6e0f3fdc437f2b2
wesleycomp <weslleycomp@gmail.com>
2020-10-11 21:48:38 -0300

    atualizando e finalizando deploy

--- wrl_infosystem/settings.py
+++ wrl_infosystem/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '4kfgz_k#7vj0=sb(_)&#0q3so6)9qfa9wmkaty30kysqqohq_3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
