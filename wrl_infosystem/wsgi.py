f8849b2ff1646707579b5ab7c1b14ab49025441b
wesleycomp <weslleycomp@gmail.com>
2020-10-11 21:37:58 -0300

    projeto finalizado

---
+++ wrl_infosystem/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for wrl_infosystem project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling, MediaCling
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wrl_infosystem.settings')
+
+application = Cling(MediaCling(get_wsgi_application()))
